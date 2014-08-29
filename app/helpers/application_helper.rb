dafdcff08a3f31a45d30d9bad54ce9196ead8fae
Grégoire Delattre <gregoire.delattre@gmail.com>
2014-08-29 13:00:01 +0000

    Add a bootstrapped layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,11 @@
 # ApplicationHelper
 module ApplicationHelper
+  # Help with selected navbar
+  def nav_link(link_text, link_path)
+    class_name = current_page?(link_path) ? 'active' : ''
+
+    content_tag(:li, class: class_name) do
+      link_to link_text, link_path
+    end
+  end
 end
