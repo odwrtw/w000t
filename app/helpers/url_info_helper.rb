093715df08039c5cd792903b86adda51c90b0c92
Grégoire Delattre <gregoire.delattre@gmail.com>
2014-09-12 10:28:05 +0000

    Http code helper

---
+++ app/helpers/url_info_helper.rb
@@ -0,0 +1,14 @@
+# UrlInfo helper
+module UrlInfoHelper
+  def pretty_http_status(url_info)
+    if url_info
+      css_class = url_info.active? ? 'success' : 'danger'
+      icon_class = url_info.active? ? 'chain' : 'chain-broken'
+      content_tag :span, class: "label label-#{css_class}" do
+         content_tag :i, url_info.http_code, class: "fa fa-#{icon_class}"
+      end
+    else
+      content_tag :span, 'Not Checked', class: 'label label-default'
+    end
+  end
+end
