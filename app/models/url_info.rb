9cf54bab1e1055c2f1ce87df8f970ee0bc8beff7
Grégoire Delattre <gregoire.delattre@gmail.com>
2014-09-14 16:40:16 +0200

    Move UrlLifeChecker complexity to the model (like a boss)

--- app/models/url_info.rb
+++ app/models/url_info.rb
@@ -21,4 +21,48 @@ class UrlInfo
     http_code >= 100 && http_code < 400 && (return true)
     false
   end
+
+  def update_http_code!
+    uri = parse_uri
+    code = head_request(uri) if uri
+    self.http_code = code ? code : 500
+    save!
+  end
+
+  def increase_number_of_checks!
+    inc(number_of_checks: 1)
+  end
+
+  def update_last_check!
+    touch(:last_check)
+  end
+
+  def update_linked_w000ts!
+    if self.active?
+      w000ts.each { |w| w.restore! }
+    else
+      w000ts.each { |w| w.archive! }
+    end
+  end
+
+  private
+
+  def parse_uri
+    URI.parse(url)
+    rescue URI::Error
+      nil
+  end
+
+  def head_request(uri)
+    response = nil
+    http = Net::HTTP.new(uri.host, uri.port)
+    puts "http #{http.inspect}"
+    http.use_ssl = (uri.scheme == 'https')
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    http.read_timeout = 10
+    http.start { |r| response = r.head(uri.path.size > 0 ? uri.path : '/') }
+    response.code
+  rescue
+    nil
+  end
 end
