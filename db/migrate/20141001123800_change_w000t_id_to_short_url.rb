c525237c538a4c6167170050ae692cba1b6791c8
Grégoire Delattre <gregoire.delattre@gmail.com>
2014-10-01 14:33:10 +0000

    Use w000t short url as _id

---
+++ db/migrate/20141001123800_change_w000t_id_to_short_url.rb
@@ -0,0 +1,28 @@
+# Change all the w000t ids
+class ChangeW000tIdToShortUrl < Mongoid::Migration
+  def self.up
+    say_with_time('Removing crap') do
+      %w(
+        75526ac69 328c81739 cf1c1e58e d3b19b5a9 c0a140ed3 dba347405 4256c6ca5
+      ).each do |id|
+        W000t.where(short_url: id).each do |w|
+          w.destroy
+        end
+      end
+    end
+
+    say_with_time('Changing id of w000t') do
+      W000t.all.each do |w|
+        # Clone this w000t
+        clone = w.clone
+        clone._id = w.short_url
+        say "#{clone.short_url} updated"
+        w.destroy
+        clone.save
+      end
+    end
+  end
+
+  def self.down
+  end
+end
