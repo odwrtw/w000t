d4c6e117d30335633620685981b1881598fc6087
Lucas BEE <pouulet@gmail.com>
2014-12-19 21:05:15 +0100

    Use default credentials in development and env in prod / staging

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,18 +2,30 @@
 
 CarrierWave.configure do |config|
   config.storage = :fog
-  config.fog_credentials = {
-    provider: 'OpenStack',
-    openstack_tenant: ENV['OPENSTACK_TENANT'] || '98091111',
-    # Your OpenStack Username
-    openstack_username: ENV['OPENSTACK_USERNAME'] || 'pouulet@gmail.com',
-    # Your OpenStack Password
-    openstack_api_key:  ENV['OPENSTACK_API_KEY'] || 'the_dummy_password!',
-    openstack_auth_url: ENV['OPENSTACK_AUTH_URL'] || 'https://auth.runabove.io/v2.0/tokens',
-    openstack_region: ENV['OPENSTACK_REGION'] || 'SBG-1'
-  }
-  config.fog_directory  = ENV['FOG_DIRECTORY'] || ENV['RAILS_ENV'] || 'test_directory'
-  config.asset_host  = ENV['OPENSTACK_ASSET_HOST'] || nil
+  if Rails.env.development?
+    config.fog_credentials = {
+      provider: 'OpenStack',
+      openstack_tenant: '98091111',
+      openstack_username: 'pouulet@gmail.com',
+      openstack_api_key:  'the_dummy_password!',
+      openstack_auth_url: 'https://auth.runabove.io/v2.0/tokens',
+      openstack_region: 'SBG-1'
+    }
+    config.fog_directory  = 'test_directory'
+  else
+    config.fog_credentials = {
+      provider: 'OpenStack',
+      openstack_tenant: ENV['OPENSTACK_TENANT'],
+      # Your OpenStack Username
+      openstack_username: ENV['OPENSTACK_USERNAME'],
+      # Your OpenStack Password
+      openstack_api_key:  ENV['OPENSTACK_API_KEY'],
+      openstack_auth_url: ENV['OPENSTACK_AUTH_URL'],
+      openstack_region: ENV['OPENSTACK_REGION']
+    }
+    config.fog_directory  = ENV['FOG_DIRECTORY']
+    config.asset_host  = ENV['OPENSTACK_ASSET_HOST']
+  end
   config.fog_public     = true         # optional, defaults to true
   config.fog_attributes = {
     'Cache-Control' => "max-age=#{365.day.to_i}"
