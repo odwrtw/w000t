9fd4487be3dbe70bca05dd699a6b3ea0e029c732
Grégoire Delattre <gregoire.delattre@gmail.com>
2014-12-21 00:53:08 +0000

    Add user dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   resources :users, param: :pseudo, only: [] do
     resources :authentication_tokens, path: :tokens
     get 'wall' => 'w000ts#image_index'
+    get 'dashboard' => 'user_dashboard#show'
   end
 
   # Personnal w000ts
---
+++ test/controllers/user_dashboard_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class UserDashboardControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
