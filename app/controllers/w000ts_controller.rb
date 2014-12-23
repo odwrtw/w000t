3a48bee31342a7c26688ca85a8fd83e19d9646a9
Grégoire Delattre <gregoire.delattre@gmail.com>
2014-12-23 12:24:43 +0000

    Small refactoring on w000ts

--- app/controllers/w000ts_controller.rb
+++ app/controllers/w000ts_controller.rb
@@ -6,20 +6,19 @@ class W000tsController < ApplicationController
   before_action :set_w000t, only:
                   [:show, :edit, :update, :destroy, :redirect, :click]
   # Auth by token before show, the informations depends on the user rights
-  before_action :w000thenticate, only: [:show]
+  before_action :w000thenticate, only: [:create, :show]
   # Actions where the user needs to be logged in
-  before_action :w000thenticate!,
-                only: [:destroy, :update, :edit, :my_index]
+  before_action :w000thenticate!, only: [:destroy, :update, :edit, :my_index]
   # Check to performs before w000t creation
-  before_action :prevent_w000tception,
-                :check_token,
-                :check_w000t,
-                only: [:create]
+  before_action :prevent_w000tception, :check_w000t, only: [:create]
   # Set the user from the routes
   before_action :set_user, only: [:image_index]
+  # Check the rights of the user on the w000t
+  before_action :check_w000t_owner, only: [:update, :destroy]
+  # Check if the type is allowed
+  before_action :check_w000t_type_params, only: [:my_index]
 
   # GET /w000ts
-  # GET /w000ts.json
   def index
     @w000ts = W000t.where(user: nil, status: :public)
                    .order_by(created_at: :desc)
@@ -27,7 +26,6 @@ class W000tsController < ApplicationController
   end
 
   # GET /w000ts/1
-  # GET /w000ts/1.json
   def show
   end
 
@@ -38,12 +36,6 @@ class W000tsController < ApplicationController
 
   # GET /w000ts/new
   def update
-    unless current_user.w000ts.find_by(short_url: @w000t.short_url)
-      return redirect_to :back, flash: {
-        alert: 'You can not update this w000t, only the owner can'
-      }
-    end
-
     if @w000t.update(w000t_update_params)
       respond_to do |format|
         format.js { render :update_w000t, locals: { w000t: @w000t } }
@@ -53,17 +45,11 @@ class W000tsController < ApplicationController
         end
       end
     else
-      respond_to do |format|
-        format.json do
-          render json: @w000t.errors, status: :unprocessable_entity
-        end
-        format.html { render action: :new }
-      end
+      render_errors
     end
   end
 
   # POST /w000ts
-  # POST /w000ts.json
   def create
     @w000t = W000t.new(w000t_params)
     @w000t.user = current_user if user_signed_in?
@@ -73,27 +59,14 @@ class W000tsController < ApplicationController
       # easy parsing
       render_create_w000t
     else
-      respond_to do |format|
-        format.json do
-          render json: @w000t.errors, status: :unprocessable_entity
-        end
-        format.html { render action: :new }
-      end
+      render_errors
     end
   end
 
   # DELETE /w000ts/1
-  # DELETE /w000ts/1.json
   # Only the w000t creator can delete is own w000t, for public w000ts, no
   # deletion for now
   def destroy
-    # Check user right
-    unless current_user.w000ts.find_by(short_url: @w000t.short_url)
-      return redirect_to :back, flash: {
-        alert: 'You can not delete this w000t, only the owner can'
-      } unless current_user.admin
-    end
-
     if @w000t.destroy
       respond_to do |format|
         format.html do
@@ -104,21 +77,18 @@ class W000tsController < ApplicationController
         format.json { head :no_content }
       end
     else
-      respond_to do |format|
-        format.json do
-          render json: @w000t.errors, status: :unprocessable_entity
-        end
-        # TODO: add html respose for errors
-      end
+      render_errors
     end
   end
 
+  # GET /:short_url
   def redirect
     @w000t.inc(number_of_click: 1)
     redirect_to @w000t.url_info_url
   end
 
-  # Add a click to the count but do not redirect
+  # GET /:short_url/click
+  # Add a click to the count but do not redirect, used to diplay w000t preview
   def click
     @w000t.inc(number_of_click: 1)
     respond_to do |format|
@@ -126,6 +96,7 @@ class W000tsController < ApplicationController
     end
   end
 
+  # GET /w000t/me
   def my_index
     @w000ts = current_user.w000ts
     unless params[:tags].blank?
@@ -133,39 +104,29 @@ class W000tsController < ApplicationController
       @tags = params[:tags]
     end
     if params[:type]
-      unless TypableUrl::TYPES.include? params[:type].to_sym
-        return redirect_to w000ts_me_path, flash: { alert: 'Invalid filter' }
-      end
       @w000ts = @w000ts.by_type(params[:type])
-                .order_by(created_at: :desc)
-                .page(params[:page]).per(25)
+                .order_by(created_at: :desc).page(params[:page]).per(25)
     else
       @w000ts = @w000ts.order_by(created_at: :desc)
                 .page(params[:page]).per(25)
     end
   end
 
-  # GET /meme
-  # GET /meme.json
+  # GET /w000ts/meme
   def my_image_index
     params[:seed] ||= Random.new_seed
     srand params[:seed].to_i
     @w000ts = Kaminari.paginate_array(
-      current_user.w000ts.by_type('image')
-                         .and(archive: 0)
-                         .ne(status: :hidden)
-                         .shuffle
+      current_user.w000ts.of_owner_wall.shuffle
     ).page(params[:page]).per(20)
   end
 
+  # GET /users/:user_pseudo/wall
   def image_index
     params[:seed] ||= Random.new_seed
     srand params[:seed].to_i
     @w000ts = Kaminari.paginate_array(
-      @user.w000ts.by_type('image')
-                         .and(archive: 0)
-                         .and(status: :public)
-                         .shuffle
+      @user.w000ts.of_public_user_wall.shuffle
     ).page(params[:page]).per(20)
   end
 
@@ -235,6 +196,25 @@ class W000tsController < ApplicationController
     render_create_w000t(:ok)
   end
 
+  # Check user right on w000t
+  def check_w000t_owner
+    action = params[:action]
+    action = 'delete' if action.eql? 'destroy'
+    unless current_user.w000ts.find_by(short_url: @w000t.short_url)
+      return redirect_to :back, flash: {
+        alert: "You can not #{action} this w000t, only the owner can"
+      } unless current_user.admin?
+    end
+  end
+
+  # Check if the given type is allowed
+  def check_w000t_type_params
+    return unless params[:type]
+    unless TypableUrl::TYPES.include? params[:type].to_sym
+      return redirect_to w000ts_me_path, flash: { alert: 'Invalid filter' }
+    end
+  end
+
   # Render the w000t creation in any format
   def render_create_w000t(status = nil)
     status ||= :created
@@ -244,6 +224,15 @@ class W000tsController < ApplicationController
       format.js   { render :create, locals: { w000t: @w000t } }
       format.html { redirect_to :back, notice: "W000t created #{w000t_url}" }
       format.text { render text: w000t_url }
+    end
+  end
+
+  def render_errors
+    respond_to do |format|
+      format.json do
+        render json: @w000t.errors, status: :unprocessable_entity
+      end
+      format.html { render action: :new }
     end
   end
 end
