5fe631b7716d48dd31fd243786357c55370cd730
Lucas BEE <pouulet@gmail.com>
2016-06-02 14:46:53 +0000

    Fix device update and rubocop it

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  protect_from_forgery with: :null_session, if: (proc do
-    |c| c.request.format == 'application/json'
+  protect_from_forgery with: :null_session, if: (proc do |c|
+    c.request.format == 'application/json'
   end)
 
   before_action :configure_permitted_parameters, if: :devise_controller?
@@ -26,7 +26,7 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :pseudo
-    devise_parameter_sanitizer.for(:account_update) << :pseudo
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:pseudo])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:pseudo])
   end
 end
