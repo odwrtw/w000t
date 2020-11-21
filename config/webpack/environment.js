1f8fbcd8b1f0c386e70b6e7c04754eedc0a6614d
Lucas BEE <pouulet@gmail.com>
2020-11-22 00:36:04 +0100

    Use webpack to handle javascript
    This is the new way for Rails 6

---
+++ config/webpack/environment.js
@@ -0,0 +1,11 @@
+const { environment } = require('@rails/webpacker')
+const coffee =  require('./loaders/coffee')
+const webpack = require('webpack');
+
+environment.loaders.prepend('coffee', coffee)
+environment.plugins.append('Provide', new webpack.ProvidePlugin({
+  $: 'jquery',
+  jQuery: 'jquery',
+  Popper: ['popper.js', 'default']
+}));
+module.exports = environment
