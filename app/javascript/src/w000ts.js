00edfb238cc84dd5729a23d73625c71e6d1b86aa
Lucas BEE <pouulet@gmail.com>
2020-12-02 11:19:31 +0100

    Update javascript

---
+++ app/javascript/src/w000ts.js
@@ -0,0 +1,123 @@
+import Tokenfield from 'bootstrap-tokenfield'
+import Masonry from 'masonry-layout'
+import imagesloaded from 'imagesloaded'
+import InfiniteScroll from 'infinite-scroll'
+
+// Define a jQuery sub that will select the text of a div
+$.fn.selectText = function() {
+  let range;
+  const doc = document;
+  const element = this[0];
+  if (doc.body.createTextRange) {
+    range = document.body.createTextRange;
+    range.moveToElementText(element);
+    return range.select();
+  } else if (window.getSelection) {
+    const selection = window.getSelection();
+    range = document.createRange();
+    range.selectNodeContents(element);
+    selection.removeAllRanges();
+    return selection.addRange(range);
+  }
+};
+
+$(document).on('turbolinks:load', function() {
+  if ($(".w000ts").length) {
+    $("a.type-youtube").click(event => $('#w000t-preview-modal').modal());
+  }
+
+  if ($(".w000ts.new").length) {
+    $(".w000t-wat").click(function(event){
+      $('.upload-image').toggle(300);
+      return $('.form-url').toggle(300);
+    });
+  }
+
+  if ($(".w000ts.owner_list").length) {
+    $("#search-toggle button").click(function(event){
+      $('#search-forms div.form').hide();
+      return $('#'+this.id+'-form').show();
+    });
+
+    new Tokenfield(document.querySelector(".tags-search"))
+    new Tokenfield(document.querySelector(".w000t-tags-form"))
+    $("div.container").delegate(".w000t-tags", "click", function() {
+      $(".tags-input-displayed").hide();
+      $(".tags-td-hidden").show();
+      const $el = $(this).closest("td").siblings(".w000t-form");
+      $el.show();
+      $el.addClass("tags-input-displayed");
+      $el.find('input').focus();
+      return $(this).hide().addClass('tags-td-hidden');
+    });
+  }
+
+  if ($('.w000ts.owner_wall, .w000ts.user_wall, .w000ts.public_wall').length ) {
+    // When we click on the current status, we show a ul with the others statuses
+    $("div.container").delegate("td.w000t-status", "click", function() {
+      return $(this).parents("figure").find(".w000t-form-status").toggle();
+    });
+
+    // When we click on the share button, we show a div with the w000ted url and
+    // select the text
+    $("div.container").delegate("a.to_share", "click", function() {
+      const $el = $(this).closest("div").siblings(".shared");
+      $el.show();
+      return $el.selectText();
+    });
+
+    // When we leave the figure, we need to hide the w000ted url that was shown by
+    // clicking on the share button and the status form
+    $("div.container").delegate("figure", "mouseleave", function() {
+      $(this).children(".shared").hide();
+      return $(this).children(".w000t-form-status").hide();
+    });
+
+    // Masonry options
+    const masonry_options = {
+      itemSelector: ".item",
+      transitionDuration: 0,
+      stamp: '.stamp',
+    };
+
+    var $container = $("div#w000t-wall-container");
+
+    imagesLoaded.makeJQueryPlugin( $ );
+
+    let count = 0;
+    var onProgress = function( instance, image ) {
+      count++;
+      // When the image is loaded, remove the hidden class
+      if (image.isLoaded) {
+        image.img.classList.remove("hidden");
+        image.img.classList.add("stamp");
+      }
+      // Every 4 images loaded, trigger the redraw of Masonry
+      if ((count % 4) === 0) {
+        var msnry = Masonry.data('div#w000t-wall-container')
+        msnry && msnry.layout();
+        return msnry;
+      }
+    }
+
+    InfiniteScroll.imagesLoaded = imagesLoaded;
+    InfiniteScroll.prototype.appendOutlayerItems = function( fragment, appendReady ) {
+      appendReady();
+      // append once images loaded
+      $container.imagesLoaded().progress(onProgress);
+    };
+
+    var msnry = new Masonry( 'div#w000t-wall-container', masonry_options);
+
+    $container.imagesLoaded().progress(onProgress);
+
+    var infScroll = new InfiniteScroll( 'div#w000t-wall-container', {
+      path : "ul.pagination li a[rel='next']",
+      loadOnScroll: true,
+      scrollThreshold: 600,
+      append: "#w000t-wall-container .item",
+      outlayer: msnry,
+      history: false,
+    });
+  }
+});
